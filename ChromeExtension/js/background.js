21b3834da87dc01c994ebcf861e94771675f825f
ToDarkClown <294203341@qq.com>
2018-08-22 09:05:54 +0800

    Add files via upload

--- ChromeExtension/js/background.js
+++ ChromeExtension/js/background.js
@@ -1,29 +1,70 @@
 ﻿ window.serverdata = [];
- chrome.browserAction.onClicked.addListener(function (tab){
-   var pagedata = null;
-  //  找到目标数据源
-   pagedata = window.serverdata.find((res) => res.source.find( (item) => item.url.indexOf(tab.url) != -1));
-   pagedata = pagedata.source.find((res) => res.url.indexOf(tab.url) != -1);
-  //  传输页面所需数据
-  if(pagedata != undefined){
-    chrome.tabs.sendMessage(tab.id, {action:"paste",currenttab: tab, serverdata: pagedata}, function(respond){
-    }); 
-  }
- })
-//  接收数据源 替换或添加到serverdata
+ window.urlAndSelect = [];
+ window.datasourcetabsid = 0;
+ window.currentdata = "";
+
+ //  Receive data source, replace or add to serverdata
  chrome.extension.onMessage.addListener(
-  function (request, sender, sendResponse) {
-      if (request.action === "server") {
-          var flag = true;
-          for(var i = 0;i<window.serverdata.length;i++){
-            if(window.serverdata[i].sourceUrl === request.source.sourceUrl){
-              window.serverdata[i] = request.source;
-                flag = false;
-            }
-          }
-          if(flag){
-            window.serverdata.push(request.source);
-          }
-      }
-  }
-);
+   function (request, sender, sendResponse) {
+     if (request.action === "server") {
+       for (let index = 0; index < window.urlAndSelect.length; index++) {
+         window.urlAndSelect[index].checkedData = -1;
+       }
+       var flag;
+       for (var i = 0; i < request.source.length; i++) {
+         flag = true;
+         for (var j = 0; j < window.serverdata.length; j++) {
+           if (request.source[i].url && (request.source[i].url == window.serverdata[j].url)) {
+             window.serverdata[i] = request.source[j];
+             flag = false;
+             break;
+           }
+         }
+         if (flag && request.source[i].url) {
+           window.serverdata.push(request.source[i]);
+         }
+
+       }
+       window.datasourcetabsid = sender.tab.id;
+     } else if (request.action === "copyover") {
+       var item;
+       window.currentdata.isCopied = true;
+
+       for (var j = 0; j < window.serverdata.length; j++) {
+         var item = window.serverdata[j].data.find((item) => item.sectionKey == window.currentdata.sectionKey);
+         if (item != undefined) {
+           window.serverdata[j].data.splice(window.serverdata[j].data.indexOf(item), 1, window.currentdata);
+           break;
+         }
+       }
+       chrome.tabs.query({
+         active: true,
+         currentWindow: true
+       }, function (tabs) {
+         chrome.tabs.sendMessage(window.datasourcetabsid, {
+           action: "triggerfunc",
+           currentdata: window.currentdata
+         }, function (response) {});
+       });
+     }
+   }
+ );
+ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+   if (window.urlAndSelect.length > 0) {
+     var getNowPage = window.urlAndSelect.find((urlItem) => urlItem.urlId == tab.id && urlItem.nowUrl == tab.url);
+     if (getNowPage) {
+       var nowUrlIndex = window.urlAndSelect.indexOf(getNowPage);
+       getNowPage.checkedData = -1;
+       window.urlAndSelect.splice(nowUrlIndex, 1, getNowPage);
+     }
+   }
+ })
+ chrome.tabs.onRemoved.addListener(function (tabId, changeInfo, tab) {
+   if (window.urlAndSelect.length > 0) {
+     var getNowPage = window.urlAndSelect.find((urlItem) => urlItem.urlId == tabId);
+     if (getNowPage) {
+       var nowUrlIndex = window.urlAndSelect.indexOf(getNowPage);
+       window.urlAndSelect.splice(nowUrlIndex, 1);
+     }
+   }
+ })
--- ChromeExtension/js/content-script.js
+++ ChromeExtension/js/content-script.js
@@ -1,78 +1,113 @@
 ﻿chrome.extension.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.action === "paste") {
+        if (request.action === "currentdata") {
             var pagedata = request.serverdata;
             var tab = request.currenttab;
-            var localHref=window.location.href;
-            // 添加数据到页面
-                if(pagedata){
-                    if(localHref.indexOf(pagedata.url)>=0){
-                        //循环判断数据源的目标标签有没有checkbox类型的有就先初始化为false
-                        for(var i=0;i<pagedata.data.length;i++){
-                            if(pagedata.data[i].id){
-                              ctrl=$("#"+pagedata.data[i].id);
-                            }
-                            else if(pagedata.data[i].name){
-                              ctrl=$("[name="+pagedata.data[i].name+"]");                    
-                            }
-                            else if(pagedata.data[i].path){
-                                ctrl=$(pagedata.data[i].path);           
-                            }   
-                            if((ctrl[0].type==="checkbox" || ctrl[0].type==="radio")){
-                                for(var k =0;k<ctrl.length;k++){
-                                    ctrl[k].checked=false;
-                                }
-
+            var localHref = window.location.href;
+            // add data to page
+            if (pagedata&&pagedata.content!=undefined) {
+                // if(localHref.indexOf(pagedata.url)>=0){
+                //The loop determines whether the target tag of the data source has a checkbox type and is initialized to false first.
+                for (var i = 0; i < pagedata.content.length; i++) {
+                    if (pagedata.content[i].id) {
+                        ctrl = $("#" + pagedata.content[i].id);
+                    } else if (pagedata.content[i].name) {
+                        ctrl = $("[name=" + pagedata.content[i].name + "]");
+                    } else if (pagedata.content[i].path) {
+                        ctrl = $(pagedata.content[i].path);
+                    }
+                    if (ctrl[0] != undefined) {
+                        if ((ctrl[0].type === "checkbox" || ctrl[0].type === "radio")) {
+                            for (var k = 0; k < ctrl.length; k++) {
+                                ctrl[k].checked = false;
                             }
                         }
-                         //循环判断数据源的目标标签有没有checkbox类型的有就先初始化为false---end
-                        for(var i=0;i<pagedata.data.length;i++){
-                            if(pagedata.data[i].id){
-                              ctrl=$("#"+pagedata.data[i].id);
-                            }
-                            else if(pagedata.data[i].name){
-                              ctrl=$("[name="+pagedata.data[i].name+"]");                    
-                            }
-                            else if(pagedata.data[i].path){
-                                ctrl=$(pagedata.data[i].path);           
-                            }          
-                            // 如果为单选或者多选
-                            if(ctrl[0].type === 'checkbox'|| ctrl[0].type === 'radio'){
-                                // 如果name相同并有多个是多选，找到并选中
-                                if(ctrl.length>1){
-                                  for(var j =0;j<ctrl.length;j++){
-                                    if(ctrl[j].value === pagedata.data[i].value){
-                                        ctrl[j].checked=true;
-                                    }
-                                  }
-                                }else{
-                                    // 如果只有一个直接选中
-                                    ctrl.prop("checked",true);
-                                }
-                            }else{
-                                // 其他的赋值
-                                ctrl.val(pagedata.data[i].value);
-                            }                      
-                        }
-                    } 
+                    }
                 }
-        } 
+                //The loop determines whether the target tag of the data source has a checkbox type and is initialized to false first.---end
+                for (var i = 0; i < pagedata.content.length; i++) {
+                    if (pagedata.content[i].id) {
+                        ctrl = $("#" + pagedata.content[i].id);
+                    } else if (pagedata.content[i].name) {
+                        ctrl = $("[name=" + pagedata.content[i].name + "]");
+                    } else if (pagedata.content[i].path) {
+                        ctrl = $(pagedata.content[i].path);
+                    }
+                    // If it is single or multiple choice
+                    if (ctrl[0] != undefined) {
+                        if (ctrl[0].type === 'checkbox' || ctrl[0].type === 'radio') {
+                            // If the name is the same and more than one is multiple choice, find and select
+                            if (ctrl.length > 1) {
+                                for (var j = 0; j < ctrl.length; j++) {
+                                    if ((ctrl[j].value === pagedata.content[i].value)) {
+                                        ctrl[j].checked = true;
+                                    }
+                                }
+                            } else {
+                                // If only one is directly selected
+                                ctrl.prop("checked", true);
+                            }
+                        } else {
+                            // Other assignments
+                                ctrl.val(pagedata.content[i].value);
+                        }
+                    }
+
+                }
+                // } 
+            }
+
+            //Notify other pages
+            chrome.runtime.sendMessage({
+                action: "copyover",
+                key: pagedata.sectionKey
+            });
+        } else if (request.action === "searchurl") {
+            chrome.runtime.sendMessage({
+                action: "geturl",
+                url: window.location.href
+            });
+        } else if (request.action === "saveCurrentCheck") {
+            chrome.runtime.sendMessage({
+                action: "saveCheck",
+                source: request.checkData
+            });
+        } else if (request.action === "triggerfunc") {
+           window.postMessage(request.currentdata, window.location.href);
+        }
     }
 );
-// 监听事件触发
-$(function () {
-    if( document.getElementById('ChromeExtensionData') != null){
-        document.getElementById('ChromeExtensionData').addEventListener('myCustomEvent', function() {
-            var localHref=window.location.href;
-            var eventData =$("#ChromeExtensionData").val();
-            var eventDatajson = '{"source":'+eventData+',"sourceUrl": "'+localHref+'"}';
-            eventDatajson = JSON.parse(eventDatajson);
-            chrome.runtime.sendMessage({
-                action: "server",
-                source: eventDatajson
-            });
-          });
+
+// Url does not match
+// 　　　　 data: {'orderinfo': sss},
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action == 'nosearchurl') {
+        alert('Invalid url!');
     }
-  })
+    if(request.action == 'noData'){
+        alert('Data is null!');
+    }
+});
 
-
+// Listening event trigger
+$(function () {
+    if (document.getElementById('ChromeExtensionData') != null) {
+        document.getElementById('ChromeExtensionData').addEventListener('myCustomEvent', function () {
+            var localHref = window.location.href;
+            var eventData = $("#ChromeExtensionData").val();
+            var eventDatajson = '{"source":' + eventData + ',"sourceUrl": "' + localHref + '"}';
+            try {
+                eventDatajson = JSON.parse(eventDatajson);
+                chrome.runtime.sendMessage({
+                    action: "server",
+                    source: eventDatajson.source,
+                },function(e){
+                    alert('data has been copied');
+                });
+            } catch (error) {
+                alert('incorrect data');
+            }
+          
+        });
+    }
+})
