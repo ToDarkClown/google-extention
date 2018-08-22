fd07115461bd5f24095bc635c19c1a7ebd580079
ToDarkClown <294203341@qq.com>
2018-08-22 08:54:43 +0800

    Add files via upload

---
+++ ChromeExtension/background.js
@@ -0,0 +1,70 @@
+﻿ window.serverdata = [];
+ window.urlAndSelect = [];
+ window.datasourcetabsid = 0;
+ window.currentdata = "";
+
+ //  Receive data source, replace or add to serverdata
+ chrome.extension.onMessage.addListener(
+   function (request, sender, sendResponse) {
+     if (request.action === "server") {
+       for (let index = 0; index < window.urlAndSelect.length; index++) {
+         window.urlAndSelect[index].checkedData = -1;
+       }
+       var flag;
+       for (var i = 0; i < request.source.length; i++) {
+         flag = true;
+         for (var j = 0; j < window.serverdata.length; j++) {
+           if (request.source[i].url && (request.source[i].url == window.serverdata[j].url)) {
+             window.serverdata[i] = request.source[j];
+             flag = false;
+             break;
+           }
+         }
+         if (flag && request.source[i].url) {
+           window.serverdata.push(request.source[i]);
+         }
+
+       }
+       window.datasourcetabsid = sender.tab.id;
+     } else if (request.action === "copyover") {
+       var item;
+       window.currentdata.isCopied = true;
+
+       for (var j = 0; j < window.serverdata.length; j++) {
+         var item = window.serverdata[j].data.find((item) => item.sectionKey == window.currentdata.sectionKey);
+         if (item != undefined) {
+           window.serverdata[j].data.splice(window.serverdata[j].data.indexOf(item), 1, window.currentdata);
+           break;
+         }
+       }
+       chrome.tabs.query({
+         active: true,
+         currentWindow: true
+       }, function (tabs) {
+         chrome.tabs.sendMessage(window.datasourcetabsid, {
+           action: "triggerfunc",
+           currentdata: window.currentdata
+         }, function (response) {});
+       });
+     }
+   }
+ );
+ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+   if (window.urlAndSelect.length > 0) {
+     var getNowPage = window.urlAndSelect.find((urlItem) => urlItem.urlId == tab.id && urlItem.nowUrl == tab.url);
+     if (getNowPage) {
+       var nowUrlIndex = window.urlAndSelect.indexOf(getNowPage);
+       getNowPage.checkedData = -1;
+       window.urlAndSelect.splice(nowUrlIndex, 1, getNowPage);
+     }
+   }
+ })
+ chrome.tabs.onRemoved.addListener(function (tabId, changeInfo, tab) {
+   if (window.urlAndSelect.length > 0) {
+     var getNowPage = window.urlAndSelect.find((urlItem) => urlItem.urlId == tabId);
+     if (getNowPage) {
+       var nowUrlIndex = window.urlAndSelect.indexOf(getNowPage);
+       window.urlAndSelect.splice(nowUrlIndex, 1);
+     }
+   }
+ })
---
+++ ChromeExtension/content-script.js
@@ -0,0 +1,113 @@
+﻿chrome.extension.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.action === "currentdata") {
+            var pagedata = request.serverdata;
+            var tab = request.currenttab;
+            var localHref = window.location.href;
+            // add data to page
+            if (pagedata&&pagedata.content!=undefined) {
+                // if(localHref.indexOf(pagedata.url)>=0){
+                //The loop determines whether the target tag of the data source has a checkbox type and is initialized to false first.
+                for (var i = 0; i < pagedata.content.length; i++) {
+                    if (pagedata.content[i].id) {
+                        ctrl = $("#" + pagedata.content[i].id);
+                    } else if (pagedata.content[i].name) {
+                        ctrl = $("[name=" + pagedata.content[i].name + "]");
+                    } else if (pagedata.content[i].path) {
+                        ctrl = $(pagedata.content[i].path);
+                    }
+                    if (ctrl[0] != undefined) {
+                        if ((ctrl[0].type === "checkbox" || ctrl[0].type === "radio")) {
+                            for (var k = 0; k < ctrl.length; k++) {
+                                ctrl[k].checked = false;
+                            }
+                        }
+                    }
+                }
+                //The loop determines whether the target tag of the data source has a checkbox type and is initialized to false first.---end
+                for (var i = 0; i < pagedata.content.length; i++) {
+                    if (pagedata.content[i].id) {
+                        ctrl = $("#" + pagedata.content[i].id);
+                    } else if (pagedata.content[i].name) {
+                        ctrl = $("[name=" + pagedata.content[i].name + "]");
+                    } else if (pagedata.content[i].path) {
+                        ctrl = $(pagedata.content[i].path);
+                    }
+                    // If it is single or multiple choice
+                    if (ctrl[0] != undefined) {
+                        if (ctrl[0].type === 'checkbox' || ctrl[0].type === 'radio') {
+                            // If the name is the same and more than one is multiple choice, find and select
+                            if (ctrl.length > 1) {
+                                for (var j = 0; j < ctrl.length; j++) {
+                                    if ((ctrl[j].value === pagedata.content[i].value)) {
+                                        ctrl[j].checked = true;
+                                    }
+                                }
+                            } else {
+                                // If only one is directly selected
+                                ctrl.prop("checked", true);
+                            }
+                        } else {
+                            // Other assignments
+                                ctrl.val(pagedata.content[i].value);
+                        }
+                    }
+
+                }
+                // } 
+            }
+
+            //Notify other pages
+            chrome.runtime.sendMessage({
+                action: "copyover",
+                key: pagedata.sectionKey
+            });
+        } else if (request.action === "searchurl") {
+            chrome.runtime.sendMessage({
+                action: "geturl",
+                url: window.location.href
+            });
+        } else if (request.action === "saveCurrentCheck") {
+            chrome.runtime.sendMessage({
+                action: "saveCheck",
+                source: request.checkData
+            });
+        } else if (request.action === "triggerfunc") {
+           window.postMessage(request.currentdata, window.location.href);
+        }
+    }
+);
+
+// Url does not match
+// 　　　　 data: {'orderinfo': sss},
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action == 'nosearchurl') {
+        alert('Invalid url!');
+    }
+    if(request.action == 'noData'){
+        alert('Data is null!');
+    }
+});
+
+// Listening event trigger
+$(function () {
+    if (document.getElementById('ChromeExtensionData') != null) {
+        document.getElementById('ChromeExtensionData').addEventListener('myCustomEvent', function () {
+            var localHref = window.location.href;
+            var eventData = $("#ChromeExtensionData").val();
+            var eventDatajson = '{"source":' + eventData + ',"sourceUrl": "' + localHref + '"}';
+            try {
+                eventDatajson = JSON.parse(eventDatajson);
+                chrome.runtime.sendMessage({
+                    action: "server",
+                    source: eventDatajson.source,
+                },function(e){
+                    alert('data has been copied');
+                });
+            } catch (error) {
+                alert('incorrect data');
+            }
+          
+        });
+    }
+})
